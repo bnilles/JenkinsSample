ceacbdf4bae876d8b97a5bf6320196e205836eae
bnilles <joseph.nilles@substack.com>
2021-08-21 13:51:13 -0700

    first

---
+++ src/main/java/com/ss/JenkinsSample/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.ss.JenkinsSample.controller;
+
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	@RequestMapping
+	public String hello() {
+		Random rand = new Random();
+		return "Hello with a random number : " + rand.nextInt()  ;
+	}
+
+}
